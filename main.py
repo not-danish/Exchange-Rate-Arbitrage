09193b4dfb17aabbf3ffae68c38221cefd6879ac
Danish Siddiqui <64173264+not-danish@users.noreply.github.com>
2022-06-15 21:28:33 +0000

    Created a basic menu

---
+++ main.py
@@ -0,0 +1,8 @@
+while True:
+  print("1. Edit Domestic Currency")
+  print("2. Edit Foreign Currency")
+  print("3. Edit Domestic Interest Rate")
+  print("4. Edit Foreign Domestic Rate")
+  print("5. Edit Future/Expected Foreign Exchange Rate")
+  print("-------------------------")
+  selection = input("Select an option from the list: ")
